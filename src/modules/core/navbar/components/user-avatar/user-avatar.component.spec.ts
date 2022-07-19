32425cf9bd52f98ee48d88994631b990d57378c9
Ernesto Bugdahl <ernestobugdahl@Ernestos-MBP.fibertel.com.ar>
2022-07-18 22:42:00 -0300

    unfinished test for user avatar component

---
+++ src/modules/core/navbar/components/user-avatar/user-avatar.component.spec.ts
@@ -0,0 +1,102 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { LoginModel } from "src/modules/authentication/models/login-model";
+import { AuthenticationService } from "src/modules/authentication/services/authentication.service";
+import { UserAvatarComponent } from "./user-avatar.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NO_ERRORS_SCHEMA } from '@angular/core'
+import { RouterTestingModule } from "@angular/router/testing";
+import { UiModule } from "src/modules/ui/ui.module";
+
+describe('UserAvatarComponent', () => {
+
+    let fakeAuthenticationService : AuthenticationService;
+    let component : UserAvatarComponent;
+    let fixture : ComponentFixture<UserAvatarComponent>;
+
+    beforeEach(async ()=> {
+        await TestBed.configureTestingModule({
+            declarations : [UserAvatarComponent],
+            imports: [ 
+                FontAwesomeModule,
+                HttpClientTestingModule,
+                RouterTestingModule,
+                UiModule
+             ],
+             providers: [
+                AuthenticationService,
+             ],
+             schemas : [NO_ERRORS_SCHEMA]
+
+        })
+        .compileComponents();
+
+        fixture = TestBed.createComponent(UserAvatarComponent);
+        component = fixture.componentInstance;
+        fakeAuthenticationService = TestBed.inject(AuthenticationService);
+        fixture.detectChanges();
+    });
+
+    it('should create compoent', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('displays firstname and lastname if the user is logged in', () => {
+        // Arrange
+        const loginModel : LoginModel = {
+            lastName : 'lastname',
+            email : 'email',
+            firstName : 'firstname',
+            id : 'asd',
+            token : 'token'
+        };
+
+        const link = fixture.debugElement.query(By.css('a'));
+        
+        // Act
+        fakeAuthenticationService.NotifyChanges(loginModel);
+
+        // Assert
+        expect(link.nativeElement.innerHTML).toBe('firstname lastname');
+    });
+
+    it('displays Iniciar Sesion if user is not logged in', () => {
+        // Arrange & Act
+        const link = fixture.debugElement.query(By.css('a'));
+
+        // Assert
+        expect(link.nativeElement.innerHTML).toBe('Iniciar Sesión');
+    });
+
+    it('Iniciar Sesion link navigates to log in', () => {
+        // Arrange & Act
+        const link = fixture.debugElement.query(By.css('a'));
+
+        let href = link.nativeElement.getAttribute('href');
+
+        // Assert
+        expect(href).toBe('/authentication/login');
+    });
+
+    it('does not navigate if user is logged in', () => {
+        // Arrange
+        const loginModel : LoginModel = {
+            lastName : 'lastname',
+            email : 'email',
+            firstName : 'firstname',
+            id : 'asd',
+            token : 'token'
+        };
+
+        const link = fixture.debugElement.query(By.css('a'));
+        
+        let href = link.nativeElement.getAttribute('href');
+        
+        // Act
+        fakeAuthenticationService.NotifyChanges(loginModel);
+
+        // Assert
+        expect(href).toBe('/authentication/login');
+    });
+});
--- src/modules/core/navbar/components/user-avatar/user-avatar.component.ts
+++ src/modules/core/navbar/components/user-avatar/user-avatar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy } from "@angular/core";
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { Subject, takeUntil } from "rxjs";
+import { LoginModel } from "src/modules/authentication/models/login-model";
 import { AuthenticationService } from "src/modules/authentication/services/authentication.service";
 
 
@@ -13,22 +14,36 @@ export class UserAvatarComponent implements OnDestroy
 {
     faUser = faUser;
     username : string = 'Iniciar Sesión';
+    loggedIn : boolean = false;
+    linkRoute : string[] = ['/authentication/login'];
+    
     private subjects : Subject<void> = new Subject();
-
     
     constructor(authenticationService : AuthenticationService) {
+        let storedUser = localStorage.getItem('user');
+
+        if(storedUser){
+            this.setLoggedInUser(JSON.parse(storedUser));
+        }
+
         authenticationService.loginSubject
             .asObservable()
             .pipe(takeUntil(this.subjects))
-            .subscribe( data => {
-                this.username = data.token ? `${data.firstName} ${data.lastName}`  : 'Iniciar Sesión';
-            });
+            .subscribe(data => {
+                this.setLoggedInUser(data);
+            }
+        );
     }
-
 
     ngOnDestroy(): void {
         this.subjects.next();
         this.subjects.complete();
     }
 
+    private setLoggedInUser(data : LoginModel){
+        if(data.token){
+            this.username = `${data.firstName} ${data.lastName}`;
+            this.linkRoute = [];
+        }
+    }
 }
